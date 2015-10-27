ca81e06a0369bb2265b4a0445539721fd048806f
Chris Malin <cmalin@proofpoint.com>
2015-10-26 22:51:54 -0700

    clean up shipit tasks, add dist-upgrade task

--- shipitfile.js
+++ shipitfile.js
@@ -1,56 +1,58 @@
 const SECRETS = require('./secrets.json');
+var chalk = require('chalk');
 
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
 
+  var servers = [];
+
+  SECRETS.servers.forEach(function(server){
+    servers.push(`${SECRETS.appUser}@${server}`);
+  });
+
   shipit.initConfig({
     default: {
       workspace: '/tmp/node-mongo-demo',
-      deployTo: '/home/ubuntu/node-mongo-demo',
+      deployTo: `/home/${SECRETS.appUser}/node-mongo-demo`,
       repositoryUrl: 'git@github.com:csmalin/node-mongo-demo.git',
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
       keepReleases: 2,
-      key: SECRETS.server.sshKeyLocation,
+      key: SECRETS.sshKeyLocation,
       shallowClone: true
     },
     production: {
-      servers: `${SECRETS.server.user}@${SECRETS.server.address}`
+      servers: servers
     }
   });
 
-  shipit.task('stopAll', function(){
-    return shipit.remote('/usr/bin/sudo -u ubuntu /usr/bin/forever stopall').then(function(){
-      shipit.emit('stopped');
+  shipit.task('refresh', function(){
+    shipit.log(chalk.green('Starting Refresh'));
+    shipit.log(chalk.green('Stopping the Server'));
+    shipit.remote('/usr/bin/sudo -u ubuntu /usr/bin/forever stopall').then(function(){
+      shipit.log(chalk.green('Running npm-install'));
+      shipit.remote('cd /home/ubuntu/node-mongo-demo/current && npm install').then(function(){
+          shipit.log(chalk.green('Restarting the server'));
+         shipit.remote('/usr/bin/sudo -u ubuntu /usr/bin/forever start /home/ubuntu/node-mongo-demo/current/bin/www');
+      });
     });
   });
 
-  shipit.task('start', function(){
-    return shipit.remote('/usr/bin/sudo -u ubuntu /usr/bin/forever start /home/ubuntu/node-mongo-demo/current/bin/www').then(function(){
-      shipit.emit('started');
+  shipit.task('dist-upgrade', function(){
+    shipit.log(chalk.green('Updating apt-get repos'));
+    shipit.remote('sudo apt-get update').then(function(){
+      shipit.log(chalk.green('running dist-upgrade'));
+      shipit.remote('sudo apt-get dist-upgrade -y').then(function(){
+        shipit.log(chalk.green('Finished upgrades'));
+      });
     });
   });
 
-  shipit.task('restart', function(){
-    shipit.start('stopAll');
-    shipit.on('stopped', function(){ shipit.start('start'); });
+  shipit.on('cleaned', function(){
+    return shipit.start('refresh');
   });
 
-  shipit.task('npmInstall', function(){
-    return shipit.remote('cd /home/ubuntu/node-mongo-demo/current && npm install').then(function(){
-      shipit.emit('npmInstalled');
-    });
-  });
-
-  shipit.on('deploy', function(){
-    return shipit.start('npmInstall');
-  });
-
-  shipit.on('rollbacked', function(){
-    return shipit.start('npmInstall');
-  });
-
-  shipit.on('npmInstalled', function(){
-    return shipit.start('restart');
+  shipit.on('rollback', function(){
+    return shipit.start('refresh');
   });
 };
